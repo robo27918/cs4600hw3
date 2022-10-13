c93ff0c34d5e57b05391df1d343ed4638318d441
Robert Toribio <roberttoribio04@gmail.com>
2022-10-13 15:50:43 -0700

    task1

---
+++ task1/task1.c
@@ -0,0 +1,15 @@
+/*
+Author: Roberto S. Toribio
+CS4600: HW3-TASK1
+Problem stmt:
+Let p, q, and e be three prime numbers. Let n = p*q. We will use (e, n) as the public key. Please
+calculate the private key d. The hexadecimal values of p, q, and e are listed in the following. It should be
+noted that although p and q used in this task are quite large numbers, they are not large enough to be secure.
+We intentionally make them small for the sake of simplicity. In practice, these numbers should be at least
+512 bits long (the one used here are only 128 bits).
+
+p = F7E75FDC469067FFDC4E847C51F452DF
+q = E85CED54AF57E53E092113E62F436F4F
+e = 0D88C3
+*/
+
